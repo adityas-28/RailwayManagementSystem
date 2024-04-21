86fa3255c87d7ebb6ec69e98a04bb1693316f04f
dishiiiii <mail2dishig@gmail.com>
2024-04-21 22:54:39 +0530

    booking
    booking_dropdown

--- trainbookingmenu.cpp
+++ trainbookingmenu.cpp
@@ -1,5 +1,8 @@
 #include "trainbookingmenu.h"
 #include "ui_trainbookingmenu.h"
+#include "mainwindow.h"
+#include <QSqlQuery>
+#include <QSqlError>
 
 trainBooking::trainBooking(QWidget *parent)
     : QDialog(parent)
@@ -12,3 +15,17 @@ trainBooking::~trainBooking()
 {
     delete ui;
 }
+
+void trainBooking::on_comboBox_currentTextChanged()
+{
+       QSqlQuery query("SELECT boarding FROM booking",mydb);
+    if (!query.exec()) {
+        qDebug() << "Error executing query:" << query.lastError().text();
+        return;
+    }
+    while (query.next()) {
+        QString cityName = query.value(0).toString();
+        ui->comboBox->addItem(cityName);
+    }
+}
+
